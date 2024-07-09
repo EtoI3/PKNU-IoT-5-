c92224ae448b82d12ea2627129f5581f6c646e1c
Juhyi <dlwngml201@naver.com>
2024-07-09 14:59:26 +0900

    app 1차 완성본  upload

--- smartboxmaui22/MapPage.xaml.cs
+++ smartboxmaui22/MapPage.xaml.cs
@@ -14,7 +14,7 @@ public partial class MapPage : ContentPage
         dataList = data;
 
         favoriteAddresses = new ObservableCollection<string>();
-        FavoritesListView.ItemsSource = favoriteAddresses;
+        //FavoritesListView.ItemsSource = favoriteAddresses;
 
         var mapsUrl = "https://www.google.co.kr/maps/?hl=ko&entry=ttu";
         webView.Source = mapsUrl;
@@ -22,34 +22,21 @@ public partial class MapPage : ContentPage
 
     private void BtnSerch_Clicked(object sender, EventArgs e)
     {
-        string address = AddressEntry.Text;
-        if (!string.IsNullOrWhiteSpace(address))
-        {
-            // 주소 검색 및 지도 업데이트 로직 구현
-            DisplayAlert("검색", $"주소 '{address}'로 검색합니다.", "확인");
-            var mapsUrl = $"https://maps.google.com/?q={Uri.EscapeDataString(address)}";
-            webView.Source = mapsUrl;
-        }
-        else
-        {
-            DisplayAlert("오류", "주소를 입력하세요.", "확인");
-        }
+       // string address = AddressEntry.Text;
+        //if (!string.IsNullOrWhiteSpace(address))
+        //{
+        //    // 주소 검색 및 지도 업데이트 로직 구현
+        //    //DisplayAlert("검색", $"주소 '{address}'로 검색합니다.", "확인");
+        //    //var mapsUrl = $"https://maps.google.com/?q={Uri.EscapeDataString(address)}";
+        //   // webView.Source = mapsUrl;
+        //}
+        //else
+        //{
+        //    DisplayAlert("오류", "주소를 입력하세요.", "확인");
+        //}
     }
 
-    private void BtnFavorite_Clicked(object sender, EventArgs e)
-    {
-        string address = AddressEntry.Text;
-        if (!string.IsNullOrWhiteSpace(address))
-        {
-            favoriteAddresses.Add(address);
-            DisplayAlert("추가됨", $"주소 '{address}'이(가) 즐겨찾기에 추가되었습니다.", "확인");
-            AddressEntry.Text = string.Empty; // 입력 창 비우기
-        }
-        else
-        {
-            DisplayAlert("오류", "주소를 입력하세요.", "확인");
-        }
-    }
+  
     private async void ImgBtnSet_Clicked(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new SettingsPage());
@@ -64,5 +51,23 @@ public partial class MapPage : ContentPage
     private async void ImgBtnMyinfo_Clicked(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new MyInfoPage());
+    }
+
+    private void RadioButtonA_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        var mapsUrl = "https://www.google.co.kr/maps/search/%EB%B6%80%EA%B2%BD%EB%8C%80+%ED%96%89%EB%B3%B5%EA%B8%B0%EC%88%99%EC%82%AC/data=!3m1!4b1?hl=ko&entry=ttu";
+        webView.Source = mapsUrl;
+    }
+
+    private void RadioButtonB_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        var mapsUrl = "https://www.google.co.kr/maps/search/%EA%B2%BD%EC%84%B1%EB%8C%80%EB%B6%80%EA%B2%BD%EB%8C%80%EC%97%AD/data=!3m1!4b1?hl=ko&entry=ttu";
+        webView.Source = mapsUrl;
+    }
+
+    private void RadioButtonC_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        var mapsUrl = "https://www.google.co.kr/maps/search/%EB%B6%80%EA%B2%BD%EB%8C%80+%EC%9A%A9%EB%8B%B9%EC%BA%A0%ED%8E%8C%EC%8A%A4/data=!3m1!4b1?hl=ko&entry=ttu";
+        webView.Source = mapsUrl;
     }
 }
